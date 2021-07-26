5f80da9db6152b41fbe2b2e546f7f8b4db1b8c08
Shreesha SR <shreesha.sr92@gmail.com>
2021-07-26 22:53:47 +0530

    ADDED: navbar styles

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -16,10 +16,11 @@ function Navbar(){
                     </ul>
                 </nav>
                 <div className="social-media-icons">
-                    <i class="fa fa-instagram" aria-hidden="true"></i>
-                    <i class="fa fa-facebook" aria-hidden="true"></i>
-                    <i class="fa fa-twitter" aria-hidden="true"></i>
-                    <i class="fa fa-linkedin" aria-hidden="true"></i>
+                    <a href="https://www.instagram.com/shreesha_sr/" target="_blank" rel="noreferrer"><i class="fa fa-instagram" aria-hidden="true"/></a>
+                    <a href="https://www.facebook.com/shreesha.sr.5/" target="_blank" rel="noreferrer"><i class="fa fa-facebook" aria-hidden="true"/></a>
+                    <a href="https://twitter.com/tweet_shreesha" target="_blank" rel="noreferrer"><i class="fa fa-twitter" aria-hidden="true"/></a>
+                    <a href="https://www.linkedin.com/in/shreesha-sr-822322100/" target="_blank" rel="noreferrer"><i class="fa fa-linkedin" aria-hidden="true"/></a>
+                    <a href="https://github.com/shreeshasr" target="_blank" rel="noreferrer"><i class="fa fa-github" aria-hidden="true"/></a>
                 </div>
             </div>
         )
